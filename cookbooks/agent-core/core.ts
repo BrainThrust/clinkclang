67981874e08503c9cd06b16142130ad09d011008
Lim Yi Sheng Brian <83122861+BrianLYS@users.noreply.github.com>
2025-01-16 15:42:16 +0800

    cleanup():

---
+++ cookbooks/agent-core/core.ts
@@ -0,0 +1,18 @@
+// Build the foundation of the agent-core
+
+// agentjo has their strictjson
+// openai has structured outpus
+// form of `structured outputs` for clinkclang
+
+// Requirements:
+// Use multiple models
+// Use structured outputs
+// No lockin to big models
+
+// Inspiration:
+// https://github.com/tanchongmin/strictjson
+
+// Build from zod:
+// https://zod.dev/
+
+// Take in either XML or JSON
