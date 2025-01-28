d105d7bf5c78e2849e30a36ce1f3362047f5478e
horouuu <fernandafadja@gmail.com>
2025-01-28 22:44:14 +0800

    MemoryInterface: Update PgVectorMemory with SessionId field

--- packages/agent-core/memory/memory-pgvector.ts
+++ packages/agent-core/memory/memory-pgvector.ts
@@ -2,21 +2,26 @@ import { Memory } from "./memory";
 import { getEmbeddingsOpenAI } from "@/agent-functions/memory/get-embeddings-openai";
 import { memoryTable } from "./db/schema";
 import { OpenAIEmbedding } from "@/agent-core/schema/memory";
-import { drizzle } from "drizzle-orm/node-postgres";
+import { drizzle, NodePgDatabase } from "drizzle-orm/node-postgres";
 import { eq, l2Distance } from "drizzle-orm";
-import { config } from "dotenv";
-config({ path: __dirname + "./../../../.env" });
 
 type PgVectorMemoryRow = {
   memory: string;
   embedding: number[];
+  session_id: string;
 };
 
 export class PgVectorMemory extends Memory {
-  dbClient = drizzle(process.env.DATABASE_URL!);
+  private dbClient: NodePgDatabase;
+
+  constructor(connectionUrl: string) {
+    super();
+    this.dbClient = drizzle(connectionUrl);
+  }
 
   async getAndFormatEmbeddings(
-    memories: string[]
+    memories: string[],
+    ssid?: string
   ): Promise<PgVectorMemoryRow[]> {
     const embeddings: OpenAIEmbedding[] = await getEmbeddingsOpenAI(memories);
     if (embeddings.length == 0) {
@@ -27,14 +32,21 @@ export class PgVectorMemory extends Memory {
     const embeddingData = embeddings.flatMap((e) =>
       e.index >= memories.length
         ? []
-        : { memory: memories[e.index]!, embedding: e.embedding }
+        : {
+            memory: memories[e.index]!,
+            embedding: e.embedding,
+            session_id: ssid ? ssid : "",
+          }
     );
 
     return embeddingData;
   }
 
-  async append(memories: string[]) {
-    const embeddingData = await this.getAndFormatEmbeddings(memories);
+  async append(memories: string[], ssid?: string) {
+    const embeddingData = await this.getAndFormatEmbeddings(
+      memories,
+      ssid ? ssid : ""
+    );
 
     try {
       await this.dbClient.insert(memoryTable).values(embeddingData);
@@ -62,10 +74,15 @@ export class PgVectorMemory extends Memory {
         .from(memoryTable)
         .orderBy(
           l2Distance(memoryTable.embedding, embeddingData[0]?.embedding!)
-        ).limit(5);
+        )
+        .limit(5);
     } catch (e) {
       console.error(e);
     }
+  }
+
+  async initialize(prompt: string, ssid?: string) {
+    this.append([prompt], ssid ? ssid : "");
   }
 
   async reset() {
