36af730286f52a63ffd8f6541505a5b1a173bad5
horouuu <fernandafadja@gmail.com>
2025-02-03 03:15:41 +0800

    Memory: Change function signature to async

--- packages/agent-core/memory/memory.ts
+++ packages/agent-core/memory/memory.ts
@@ -1,12 +1,17 @@
+export type MemoryFragment = {
+  memory: string;
+  sessionId: string;
+};
 export abstract class Memory {
-  abstract append(memory: string[]): void;
+  abstract append(memory: string[]): Promise<void>;
 
-  abstract remove(memory: string): void;
+  abstract remove(memory: string): Promise<void>;
+  
   removeMany(memories: string[]): void {
     memories.forEach((m) => this.remove(m));
   }
 
-  abstract reset(): void;
+  abstract reset(): Promise<void>;
 
-  abstract retrieve(task: string): void;
+  abstract retrieve(task: string): Promise<MemoryFragment[]>;
 }
