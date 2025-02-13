4f08efd286232d96f491e835d112b65d31b90ae7
Lim Yi Sheng Brian <83122861+BrianLYS@users.noreply.github.com>
2025-02-13 17:59:35 +0800

    feat(branches and gitignore):

--- packages/cli/src/config.ts
+++ packages/cli/src/config.ts
@@ -1,8 +1,12 @@
 export const remoteComponentMapping: Record<string, string> = {
-	'agent-core': 'github:BrainThrust/clinkclang/packages/agent-core',
-	'agent-evals': 'github:BrainThrust/clinkclang/packages/agent-evals',
-	examples: 'github:BrainThrust/clinkclang/packages/examples',
-	express: 'github:BrainThrust/clinkclang-express'
+	'agent-core': 'github:BrainThrust/clinkclang/packages/agent-core#dev-0.01',
+	'agent-evals': 'github:BrainThrust/clinkclang/packages/agent-evals#dev-0.01',
+	'agent-examples': 'github:BrainThrust/clinkclang/packages/agent-examples#dev-0.01',
+	'agent-functions': 'github:BrainThrust/clinkclang/packages/agent-functions#dev-0.01',
+	'agent-strategies': 'github:BrainThrust/clinkclang/packages/agent-strategies#dev-0.01',
+	'agent-tools': 'github:BrainThrust/clinkclang/packages/agent-tools#dev-0.01',
+	'agent-workflows': 'github:BrainThrust/clinkclang/packages/agent-workflows#dev-0.01'
+	// express: 'github:BrainThrust/clinkclang-express#dev-0.01'
 };
 
 export interface DegitOptions {
