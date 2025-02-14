dddabc5f1b267a82d995fd623cf2e7f68f922dff
horouuu <fernandafadja@gmail.com>
2025-02-15 01:26:03 +0800

    Workflow: Removed most execution logic from Step

--- packages/workflow/step.ts
+++ packages/workflow/step.ts
@@ -1,65 +1,21 @@
-import { z, ZodFunction, ZodTypeAny } from 'zod';
-
-type StepContextFunction = (...args: any[]) => unknown;
-
-export enum ExecutionStatus {
-	Success,
-	Failed,
-	Retry
-}
-
-export type ExecutionOutput = {
-	stepId: string;
-	status: ExecutionStatus;
-	output: unknown;
-};
+import { ZodTypeAny, z } from 'zod';
+import { ExecutionOutput, WorkflowContext, ExecutionStatus } from './types';
 
 type StepConfig = {
 	stepId: string;
-	execute: StepContextFunction;
+	execute: (ctx: WorkflowContext) => Promise<any>;
 	argsSchema?: ZodTypeAny[];
 	returnSchema?: ZodTypeAny;
 };
 
 export class Step {
 	stepId: string;
-	stepFunction: StepContextFunction;
-	stepFunctionSchema: ZodFunction<any, any>;
-	argsSchema: ZodTypeAny[];
-	returnSchema: ZodTypeAny;
+	stepFunction: <TFuncOut = any>(
+		ctx: WorkflowContext
+	) => Promise<TFuncOut extends z.ZodSchema ? z.infer<TFuncOut> : unknown>;
 
 	constructor(config: StepConfig) {
-		this.argsSchema = config.argsSchema ?? [];
-		this.returnSchema = config.returnSchema ?? z.void();
 		this.stepId = config.stepId;
-
-		if (this.argsSchema.length == 0) {
-			this.stepFunctionSchema = z.function().returns(this.returnSchema);
-		} else {
-			this.stepFunctionSchema = z
-				.function()
-				.args(this.argsSchema[0] ?? z.undefined(), ...this.argsSchema.slice(1))
-				.returns(z.promise(this.returnSchema));
-		}
-
-		this.stepFunction = this.stepFunctionSchema.implement(config.execute);
-	}
-
-	async execute(...inputs: any): Promise<ExecutionOutput> {
-		try {
-			const output = await this.stepFunction(...inputs);
-			return {
-				stepId: this.stepId,
-				status: ExecutionStatus.Success,
-				output: output
-			};
-		} catch (e) {
-			console.error(e);
-			return {
-				stepId: this.stepId,
-				status: ExecutionStatus.Failed,
-				output: null,
-			};
-		}
+		this.stepFunction = config.execute;
 	}
 }
